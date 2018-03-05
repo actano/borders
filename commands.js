bc05b0123d8e0a239cf1977d7367491ac7d77a05
thrucker <t.rucker@xpure.de>
2018-03-05 18:51:41 +0100

    Fix command export

--- commands.js
+++ commands.js
@@ -1,4 +1,4 @@
 module.exports = {
   commandWithStackFrame: require('./lib/stack-frame').commandWithStackFrame,
-  lazy: require('./lib/lazy/command'),
+  lazy: require('./lib/lazy/command').default,
 }

be77ecfaf565214e568997ca5db9ecf2a862067f
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-07-13 18:48:07 +0200

    Add compatibility tests with different libraries

---
+++ test/promise-compatibility.js
@@ -0,0 +1,82 @@
+import Bluebird from 'bluebird'
+import thenify from 'thenify'
+import { expect } from 'chai'
+import Context from '../src/context'
+
+const catchMeIfYouCan = fn => async () => {
+  let unhandled = null
+  let handled = null
+  process.on('unhandledRejection', (reason, promise) => {
+    unhandled = { reason, promise }
+  })
+  try {
+    await fn()
+  } catch (e) {
+    handled = e
+  } finally {
+    process.removeAllListeners('unhandledRejection')
+  }
+  expect(unhandled, 'Unhandled rejection found').to.eq(null)
+  expect(handled, 'Rejection not handled').to.not.eq(null)
+}
+
+describe('promise incompatibilities', () => {
+  let listeners
+
+  before('remove process listener', () => {
+    listeners = process.listeners('unhandledRejection')
+    process.removeAllListeners('unhandledRejection')
+  })
+
+  after('re-attach process listener', () => {
+    for (const listener of listeners) {
+      process.on('unhandledRejection', listener)
+    }
+  })
+
+  function createTests(resolve, reject) {
+    it('should handle reject without borders', catchMeIfYouCan(async () => {
+      await reject(new Error('Rejected'))
+    }))
+
+    it('should handle resolve and reject without borders', catchMeIfYouCan(async () => {
+      await resolve()
+      await reject(new Error('Rejected'))
+    }))
+
+    it('should handle reject in borders context', catchMeIfYouCan(async () => {
+      await new Context().execute(function* test() {
+        yield reject(new Error('Reject'))
+      }())
+    }))
+
+    it('should handle resolve followed by reject in borders context', catchMeIfYouCan(async () => {
+      await resolve()
+      await new Context().execute(function* test() {
+        yield reject(new Error('Reject'))
+      }())
+    }))
+  }
+
+  describe('Native', () => {
+    createTests(Promise.resolve, Promise.reject)
+  })
+
+  // This does not work in should handle resolve followed by reject in borders context
+  describe('Bluebird', () => {
+    createTests(Bluebird.resolve, Bluebird.reject)
+  })
+
+  describe('Native.resolve/Bluebird.reject', () => {
+    createTests(Promise.resolve, Bluebird.reject)
+  })
+
+  describe('Bluebird.resolve/Native.reject', () => {
+    createTests(Bluebird.resolve, Promise.reject)
+  })
+
+  describe('Bluebird.resolve/thenify.reject', () => {
+    const fail = (err, cb) => cb(err)
+    createTests(Bluebird.resolve, thenify(fail))
+  })
+})
--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,7 @@
+import Bluebird from 'bluebird'
+import thenify from 'thenify'
 import { expect } from 'chai'
-import { isGenerator } from '../src/utils'
+import { isPromise, isGenerator } from '../src/utils'
 
 describe('utils', () => {
   describe('isGenerator', () => {
@@ -16,6 +18,38 @@ describe('utils', () => {
     it('should not detect an iterator', () => {
       const iterator = [][Symbol.iterator]()
       expect(isGenerator(iterator)).to.eq(false)
+    })
+  })
+
+  describe('promise', () => {
+    it('should detect a native resolved promise', () => {
+      const p = Promise.resolve()
+      expect(isPromise(p)).to.eq(true)
+    })
+    it('should detect a native rejected promise', () => {
+      const p = Promise.reject(new Error('native'))
+      p.catch(() => {})
+      expect(isPromise(p)).to.eq(true)
+    })
+    it('should detect a bluebird resolved promise', () => {
+      const p = Bluebird.resolve()
+      expect(isPromise(p)).to.eq(true)
+    })
+    it('should detect a bluebird rejected promise', () => {
+      const p = Bluebird.reject(new Error('bluebird'))
+      p.catch(() => {})
+      expect(isPromise(p)).to.eq(true)
+    })
+    it('should detect a thenify resolved promise', () => {
+      const fn = thenify((cb) => { cb() })
+      const p = fn()
+      expect(isPromise(p)).to.eq(true)
+    })
+    it('should detect a thenify rejected promise', () => {
+      const fn = thenify((cb) => { cb(new Error('thenify')) })
+      const p = fn()
+      p.catch(() => {})
+      expect(isPromise(p)).to.eq(true)
     })
   })
 })

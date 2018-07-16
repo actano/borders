e0f05135e8c25a3df7827c5be99ccd5b887706ef
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-07-16 10:55:38 +0200

    Add stackframe support to promise/parallel

--- src/parallel-command.js
+++ src/parallel-command.js
@@ -1,6 +1,8 @@
+import { commandWithStackFrame } from './stack-frame'
+
 export const TYPE_PARALLEL = 'PARALLEL'
 
-export default values => ({
+export default commandWithStackFrame(values => ({
   type: TYPE_PARALLEL,
   payload: Array.from(values),
-})
+}))

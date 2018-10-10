58e5d9d605a54e7c65f28b6117bcd1275f568de1
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-10-10 19:09:29 +0200

    Test detection for yield-to-event-loop

--- src/yield-to-event-loop.js
+++ src/yield-to-event-loop.js
@@ -3,6 +3,11 @@
 // contain i/o are immediately resolved afterwards on the micro-task queue before any other
 // task has the chance to run.
 
-export default async () => new Promise((resolve) => {
-  setImmediate(resolve)
-})
+// check, if somebody (e.g. sinon) hijacked setImmediate.
+// If so, just stay in Promise-microtask land, to not break tests
+
+const originalSetImmediate = setImmediate
+
+export default () => (originalSetImmediate === setImmediate
+  ? new Promise((resolve) => { setImmediate(resolve) })
+  : Promise.resolve())

5d5f6ed659dbb02f806f503f6b505d5b05a797ff
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-10-10 15:26:31 +0200

    Move registration inside private class

--- src/context.js
+++ src/context.js
@@ -1,75 +1,15 @@
-import assert from 'assert'
-import { deprecate } from 'util'
 import Executor from './execute'
-import { evaluateWithStackFrame } from './stack-frame'
-import getCommands from './get-commands'
-import { isFunction } from './utils'
-
-export const CREATE_INITIAL_CONTEXT = '_CREATE_INITIAL_CONTEXT'
-
-const deprecateInitialContext = deprecate(() => {
-}, 'using initial context is deprecated, use this with prototype to hide non-command-functions')
 
 export default class Context {
   constructor() {
     this._commands = new Executor()
-    this._id = 0
   }
 
   use(...backends) {
-    assert(backends.length > 0, 'Must provide at least one backend')
-    const commands = getCommands(backends[0])
-    assert(commands.filter(op => this._commands[op]).length === 0, `Commands already bound: ${commands.filter(op => this._commands[op]).join(', ')}`)
-
-    const backendData = backends.map((backend) => {
-      let context
-      if (backend[CREATE_INITIAL_CONTEXT]) {
-        deprecateInitialContext()
-        context = backend[CREATE_INITIAL_CONTEXT]()
-      } else {
-        context = backend
-      }
-      this._id += 1
-      const key = `_backend.${this._id}`
-      this._commands[key] = context
-      const invoker = (fn, next) => {
-        if (fn.length === 1) {
-          return function directCommand(payload) {
-            return fn.call(this[key], payload)
-          }
-        }
-        return function contextCommand(payload) {
-          const execute = (value, subcontext) => {
-            const { stackFrame } = payload
-            const _ctx = subcontext
-              ? Object.create(this, { [key]: { value: subcontext } })
-              : this
-
-            return _ctx.execute(evaluateWithStackFrame(stackFrame, value))
-          }
-          const commandContext = { execute }
-          if (next) {
-            commandContext.next = () => next.call(this, payload)
-          }
-          return fn.call(this[key], payload, commandContext)
-        }
-      }
-
-      return { backend, invoker }
-    })
-
-
-    for (const op of commands) {
-      this._commands[op] = backendData.reduceRight((previousValue, { backend, invoker }) => {
-        const fn = backend[op]
-        if (!fn) return null
-        assert(isFunction(fn), `command.type "${op}" must be a function`)
-        assert(fn.length <= 2, `command.type "${op}" must take max two arguments (not ${fn.length})`)
-        return invoker(fn, previousValue)
-      }, null)
-    }
+    this._commands._use(...backends)
     return this
   }
+
   async execute(value) {
     return this._commands.execute(value)
   }
--- src/execute.js
+++ src/execute.js
@@ -1,10 +1,12 @@
 import assert from 'assert'
 import { deprecate } from 'util'
+import { CREATE_INITIAL_CONTEXT } from './context'
+import getCommands from './get-commands'
 import { TYPE_ITERATE } from './iterate-command'
 import iteratorToAsync from './iterator-to-async'
 import { TYPE_MAP } from './map-command'
 import { TYPE_PARALLEL } from './parallel-command'
-import { withStackFrame } from './stack-frame'
+import { evaluateWithStackFrame, withStackFrame } from './stack-frame'
 import './symbol-async-iterator'
 import { isGenerator, isString } from './utils'
 import valueType, { ARRAY, COMMAND, ITERABLE, ITERATOR } from './value-type'
@@ -16,6 +18,8 @@ const deprecateIterator = deprecate(() => {
 }, 'yielding an iterator is deprecated, yield `iterate` or `parallel` commands instead')
 const deprecateArray = deprecate(() => {
 }, 'yielding an array is deprecated, yield `iterate` or `parallel` commands instead')
+const deprecateInitialContext = deprecate(() => {
+}, 'using initial context is deprecated, use this with prototype to hide non-command-functions')
 
 function* mapCollection(self, collection, iteratee) {
   for (const item of collection) {
@@ -30,6 +34,66 @@ function* mapCollection(self, collection, iteratee) {
 }
 
 class Executor {
+  constructor() {
+    let _id = 0
+
+    this._connect = (...backends) => {
+      assert(backends.length > 0, 'Must provide at least one backend')
+
+      const connectBackend = (backend, nextBackend) => {
+        let context
+        if (backend[CREATE_INITIAL_CONTEXT]) {
+          deprecateInitialContext()
+          context = backend[CREATE_INITIAL_CONTEXT]()
+        } else {
+          context = backend
+        }
+        _id += 1
+        const key = `_backend.${_id}`
+        this[key] = context
+        const invoker = (fn, next) => {
+          if (fn.length === 1) {
+            return function directCommand(payload) {
+              return fn.call(this[key], payload)
+            }
+          }
+          return function contextCommand(payload) {
+            const execute = (value, subcontext) => {
+              const { stackFrame } = payload
+              const _ctx = subcontext
+                ? Object.create(this, { [key]: { value: subcontext } })
+                : this
+
+              return _ctx.execute(evaluateWithStackFrame(stackFrame, value))
+            }
+            const commandContext = { execute }
+            if (next) {
+              commandContext.next = () => next.call(this, payload)
+            }
+            return fn.call(this[key], payload, commandContext)
+          }
+        }
+
+        const result = {}
+        for (const op of getCommands(backend)) {
+          const fn = backend[op]
+          assert(fn.length <= 2, `command.type "${op}" must take max two arguments (not ${fn.length})`)
+          result[op] = invoker(fn, nextBackend && nextBackend[op])
+        }
+        return result
+      }
+
+      return backends.reduceRight((prev, backend) => connectBackend(backend, prev), null)
+    }
+
+    this._use = (...backends) => {
+      assert(backends.length > 0, 'Must provide at least one backend')
+      const commands = getCommands(backends[0])
+      assert(commands.filter(op => this[op]).length === 0, `Commands already bound: ${commands.filter(op => this[op]).join(', ')}`)
+      Object.assign(this, this._connect(...backends))
+    }
+  }
+
   [TYPE_PARALLEL](payload) {
     return Promise.all(payload.map(v => this.execute(v)))
   }
@@ -43,11 +107,11 @@ class Executor {
     return iteratorToAsync(mapCollection(this, collection, iteratee))
   }
 
-  async [COMMAND](value) {
+  async [COMMAND](value, backend = this) {
     const { type, payload, stackFrame } = value
     assert(isString(type), 'command.type must be string')
     assert(type[0] !== '_', `command.type "${type}" must not start with _`)
-    const res = withStackFrame(stackFrame, () => this[type](payload))
+    const res = withStackFrame(stackFrame, () => backend[type].call(this, payload))
     if (isGenerator(res) && type !== TYPE_ITERATE && type !== TYPE_MAP && type !== TYPE_PARALLEL) {
       throw new Error('implementing a command as generator is deprecated, call execute (2nd parameter) instead')
     }

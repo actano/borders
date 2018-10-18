01e71f57658b64b1bccfb6ee529d3fd5f583f1fa
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-10-18 11:34:50 +0200

    Multiplex Backend with tests

--- test/backends-multiplex.js
+++ test/backends-multiplex.js
@@ -1,26 +1,62 @@
-import assert from 'assert'
-import sinon from 'sinon'
 import { expect } from 'chai'
+import sinon from 'sinon'
+import getCommands from '../get-commands'
 
 import MultiplexBackend from '../src/backends/multiplex'
 import Context from '../src/context'
 import { echoCommand } from './_execute'
 
 describe('backends/multiplex', () => {
-  it('should call sync `selectBackend` with payload and command type', async () => {
-    const selectBackend = () => ''
+  const supportedCommands = ['echo']
+
+  let backend = null
+  let selected = null
+  let selectSpy = null
+  let createBackendSpy = null
+  let context = null
+
+  beforeEach(() => {
+    const selectBackend = () => {
+      selected = 'myBackend'
+      return selected
+    }
     const createBackend = () => ({
       echo() {},
     })
-    const selectSpy = sinon.spy(selectBackend)
-    const context = new Context().use(new MultiplexBackend(selectSpy, createBackend, ['echo']))
+    createBackendSpy = sinon.spy(createBackend)
+    selectSpy = sinon.spy(selectBackend)
+    backend = new MultiplexBackend(selectSpy, createBackendSpy, supportedCommands)
+    context = new Context().use(backend)
+  })
+
+  it('should call sync `selectBackend` with payload and command type', async () => {
     const cmd = echoCommand('a')
     await context.execute(cmd)
     expect(selectSpy.callCount).to.eq(1)
     expect(selectSpy.calledOnceWith(cmd.payload, cmd.type)).to.eq(true)
   })
-  it('should call async `createBackend` with return value from selectBackend')
-  it('should call `createBackend` only once per backend')
-  it('should register all commands of `supportedCommands`')
+
+  it('should call async `createBackend` with return value from selectBackend', async () => {
+    await context.execute(echoCommand('a'))
+    expect(createBackendSpy.callCount).to.eq(1)
+    expect(createBackendSpy.calledOnceWith(selected)).to.eq(true)
+  })
+
+  it('should call `selectBackend` once per command', async () => {
+    await context.execute(echoCommand('a'))
+    await context.execute(echoCommand('b'))
+    expect(selectSpy.callCount).to.eq(2)
+  })
+
+  it('should call `createBackend` only once per backend', async () => {
+    await context.execute(echoCommand('a'))
+    await context.execute(echoCommand('b'))
+    expect(createBackendSpy.callCount).to.eq(1)
+  })
+
+  it('should register all commands of `supportedCommands`', async () => {
+    expect(getCommands(backend)).to.deep.eq(supportedCommands)
+  })
+
   it('should delegate commands to selected backend')
 })

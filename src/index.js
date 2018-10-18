417f1b086fc5dab62f802ddf28e10a51d30b7cee
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-10-18 21:25:50 +0200

    Remove deprecated exports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import { deprecate } from 'util'
-
-import _parallel from './parallel-command'
-import _iterate from './iterate-command'
-import _map from './map-command'
-import _getCommands from './get-commands'
-
 export { default } from './context'
-
-export const parallel = deprecate(_parallel, 'import { parallel } from \'borders/commands\'')
-export const iterate = deprecate(_iterate, 'import { iterate } from \'borders/commands\'')
-export const map = deprecate(_map, 'import { map } from \'borders/commands\'')
-
-export const getCommands = deprecate(_getCommands, 'import { getCommands } from \'borders/backends\'')

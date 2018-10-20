9a8f1c4f90aae3ba34e40823261f78e68a260917
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-10-20 22:21:17 +0200

    Drop command-multiplex and chain backends

--- src/backends/index.js
+++ src/backends/index.js
@@ -1,4 +1,2 @@
 export { default as multiplex } from './multiplex'
-export { default as CommandMultiplexBackend } from './command-multiplex'
-export { default as ChainBackend } from './chain'
 export { default as getCommands } from './get-commands'
